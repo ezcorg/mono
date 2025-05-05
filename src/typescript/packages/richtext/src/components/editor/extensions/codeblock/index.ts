d4d3a80f0be6e5b77c04445ae766d26b3a450ee1
theodore brockman <tmanbrock@gmail.com>
2025-05-04 22:23:25 -0400

    Add workspace.

--- src/typescript/packages/richtext/src/components/editor/extensions/codeblock/index.ts
+++ src/typescript/packages/richtext/src/components/editor/extensions/codeblock/index.ts
@@ -42,6 +42,36 @@ export const CodeblockExtension = Node.create({
         };
     },
 
+
+    addStorage() {
+        return {
+            markdown: {
+                serialize(state, node) {
+
+                    if (node.attrs.file) {
+                        state.write(`\`\`\`${node.attrs.file}\n`);
+                    } else {
+                        state.write("```" + (node.attrs.language || "") + "\n");
+                    }
+                    state.text(node.textContent, false);
+                    state.ensureNewLine();
+                    state.write("```");
+                    state.closeBlock(node);
+                },
+                parse: {
+                    setup(markdownit) {
+                        markdownit.set({
+                            langPrefix: this.options.languageClassPrefix ?? 'language-',
+                        });
+                    },
+                    updateDOM(element) {
+                        element.innerHTML = element.innerHTML.replace(/\n<\/code><\/pre>/g, '</code></pre>')
+                    },
+                },
+            }
+        }
+    },
+
     // How to parse this node from HTML
     parseHTML() {
         return [
