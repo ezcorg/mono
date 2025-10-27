6b078abcb51089251e1e94211133ca56d568978e
Theodore Brockman <iam@theo.lol>
2025-10-27 19:28:37 -0400

    Missing format.

--- src/typescript/codeblock/vite.config.ts
+++ src/typescript/codeblock/vite.config.ts
@@ -86,6 +86,9 @@ export default async function getConfig() {
                 ]
             }
         },
+        worker: {
+            format: 'es',
+        },
         plugins: [
             snapshot({
                 gitignore: false,
