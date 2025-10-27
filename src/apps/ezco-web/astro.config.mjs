0e2f6232ea9db3beb672e0889adfe220ce6f6f43
Theodore Brockman <iam@theo.lol>
2025-10-27 17:43:13 -0400

    Update tailwindcss integration.

--- src/apps/ezco-web/astro.config.mjs
+++ src/apps/ezco-web/astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig, envField } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    vite: { plugins: [tailwindcss()], },
     site: 'https://tbrockman.github.io',
     base: '/',
     output: 'static',
@@ -14,7 +14,9 @@ export default defineConfig({
         schema: {
             DEV: envField.boolean({
                 default: false,
-                description: 'Is the app running in development mode?'
+                description: 'Is the app running in development mode?',
+                context: "client",
+                access: "public",
             }),
         }
     }
